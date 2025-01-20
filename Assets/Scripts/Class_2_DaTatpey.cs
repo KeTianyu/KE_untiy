2693d2eac458aa01656c1cf3e342aab2e2e22450
KeTianyu <jk12544521@gmail.com>
2025-01-20 16:05:44 +0800

    update 認識四大常用資料類型

---
+++ Assets/Scripts/Class_2_DaTatpey.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+// 三條斜線是摘要 (XML語言) 用來做簡短說明，可加在類別與變數上方
+/// <summary>
+/// 認識 C# 資料類型
+/// </summary>
+public class Class_2_DaTatpey:MonoBehaviour
+{
+    // 變數語法:
+    // 修飾詞 資料類型 變數名稱 指定 預設值;
+    // 四大常用資料類型
+    // 整  數 : 儲存沒有小數點的數值 int 
+    // 浮點數 : 儲存有小數點的數值 float
+    // 字  串 : 儲存文字，必須用雙引號 string
+    // 布林值 : 儲存正與反 bool 只用兩種值 true 與 false
+    public int count = 7;
+    public float movespeed = 4.5f;     //浮點數必須添加F後綴
+    public float turnspeed = 30.7f;   //大小 F 皆可
+    public string cgaracter = "蓋倫";
+    public bool isDead = false; 　   //是否死亡:否
+    public bool gameOver = true;    //是否結束:是
+    
+}
--- Assets/Scripts/Classs_1_VArfable.cs
+++ Assets/Scripts/Classs_1_VArfable.cs
@@ -6,11 +6,12 @@
  *第三行註解
 */
 
-    //引用 untiy 遊戲引擎的函示庫(開發遊戲的程式集合，命名空間)
+//引用 untiy 遊戲引擎的函示庫(開發遊戲的程式集合，命名空間)
 using UnityEngine;
 
-    // 修飾詞 類別關鍵字(腳本) 類別名稱(腳本名稱)
-public class NewEmptyCSharpScript
+// 修飾詞 類別關鍵字(腳本) 類別名稱(腳本名稱)
+//在腳本後添加:MonoBehaviour 可以讓此腳本掛在untiy的物件上
+public class Classs_1_VArfable : MonoBehaviour 
 {
     //程式內的括號都是成對出現{} () [] '' ""
     //class {} 此類別的程式內容
@@ -20,10 +21,14 @@ public class NewEmptyCSharpScript
     //定義一個記憶體內的空間用來存放資訊
 
     //變數語法:
-    //變數資料類型 變數名稱;
+    //修飾詞 變數資料類型 變數名稱;
+    //修飾詞 變數資料類型 變數名稱 指定 預設值;(指定 預設值 可省略)
     //資料類型:告訴記憶體可以放入哪些資料，例如:整數 int 可以放沒有小數點的數值
     //變數名稱:習慣用小寫開頭命名，駝峰式命名，規則與腳本名稱相同
-    int coin;
+    //公開:允許其他類別存取 public
+    //私人:禁止其他類別存取 private (預設)
+    public int coin = 500;
+    private int level = 3;
 }
 
 //類別外，不要將程式寫在這裡
