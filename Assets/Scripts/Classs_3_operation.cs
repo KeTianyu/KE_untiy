9776f090eae87e033cb9188e8f489617b596b44c
KeTianyu <jk12544521@gmail.com>
2025-01-22 13:50:09 +0800

    update 認識算術與比較運算子

--- Assets/Scripts/Classs_3_operation.cs
+++ Assets/Scripts/Classs_3_operation.cs
@@ -26,7 +26,8 @@ public class Classs_3_operator : MonoBehaviour
     public string itemDescription = "這裡是道具的描述，這裡是道具的描述，這裡是道具的描述";
 
     // 多個欄位屬性
-    [Header("血量")][Range(0, 999)]
+    [Header("血量")]
+    [Range(0, 999)]
     public int hp = 100;
     [Header("攻擊力"), Range(0, 100)]
     public float attack = 30.5f;
@@ -44,27 +45,58 @@ public class Classs_3_operator : MonoBehaviour
     // 喚醒事件 : 播放遊戲後會執行第一個事件，只執行一次
     // 事件名稱是藍色的 (沒有變色不引響，是程式本身的問題)
     private void Awake() //Awake(喚醒)
+    #region 輸出訊息
     {
         // 將小括號內的訊息輸出到 untiy 的 console 控制台
         //ctrl + Shift + 開啟控制台面板
         Debug.Log("哈囉，沃德"); // 輸出文字
         Debug.Log(hp);          // 輸出變數的值 
-        Debug.Log("攻擊力:" + attack); //字串宇變數
+        Debug.Log("攻擊力:" + attack); //字串與變數
         Debug.Log($"攻擊力:{attack}");// $字串格式
         Debug.Log("攻擊力:{attack}"); // 沒有寫$
-
 
         Debug.Log("<b>粗體</b>");
         Debug.Log("<color=orange>橘色</color>");
         // 色碼 : #紅紅綠綠藍藍，數值 : 0 ~ 9 ， a ~ f 越右邊顏色越亮 #000 黑 #fff 白
         Debug.Log("<color=#66aaff>藍色</color>");
         Debug.Log("<color=#6af>藍色</color>");
+        #endregion
+        {
+            #region 算術運算子
+            Debug.Log("color=#f93>--- 算術運算子 ---</color");
+            //運算子:
+            //1.算術運算子
+            //加、減、乘、除、餘
+            //+、-、*、/、%
+            Debug.Log(10 + 3); //13
+            Debug.Log(10 - 3); //7
+            Debug.Log(10 * 3); //30
+            Debug.Log(10 / 3); //3
+            Debug.Log(10 % 3); // 10 / 3 = 3餘1， 結果 
+            Debug.Log("<color=#f93>--- 算術運算子:區域變數 ---</color>");
+            //區域變數，僅在此大括號內存取 (不需要修飾詞的變數)
+            float numberA = 10;
+            float numberB = 3;
+            Debug.Log(numberA * numberB);
+            Debug.Log(numberA + numberB);
+            Debug.Log(numberA - numberB);
+            Debug.Log(numberA / numberB);      // 3.333
+            Debug.Log(numberA % numberB);
+            #endregion
+
+            Debug.Log("<color=#f93>--- 比較運算子 ---</color>");
+            int numberC = 100, numberD = 1;
+            
+            Debug.Log(numberC > numberD);  // 大 於 True
+            Debug.Log(numberC < numberD);  // 小 於 False
+            Debug.Log(numberC >= numberD); // 大於等於 True
+            Debug.Log(numberC <= numberD); // 小於等於 False
+            Debug.Log(numberC == numberD); // 等 於 False
+            Debug.Log(numberC != numberD); // 不等於 True
 
 
+
+
+        }
     }
-
-
-
-
-
 }
